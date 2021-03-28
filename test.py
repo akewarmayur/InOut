b1287863be4adf8f51c053f25018133e534b4cd9
Mayur Akewar <31464781+akewarmayur@users.noreply.github.com>
2021-03-28 23:20:07 +0530

    Update test.py

--- test.py
+++ test.py
@@ -10,3 +10,4 @@ for column_name in column_names:
      final_list = [x for x in final_list if (x < mn + 2 * sd)]
      list_of_outliers = list(set(df[column_name].values.tolist()) - set(final_list))
      print(list_of_outliers)
+     print('Hello')
