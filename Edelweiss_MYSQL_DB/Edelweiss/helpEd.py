4e52c4bd9217229881c491eb69f4955f7b9c3f4b
sendersender516 <84020156+sendersender516@users.noreply.github.com>
2021-05-28 10:16:23 +0530

    added main project

---
+++ Edelweiss_MYSQL_DB/Edelweiss/helpEd.py
@@ -0,0 +1,238 @@
+import numpy as np
+from common.sheetOperations import SheetOps
+import Edelweiss.edleConfig as edleConfig
+from common.common import CommonFunctions
+import config
+import warnings
+warnings.filterwarnings("ignore")
+
+class HelpEd:
+
+    def __init__(self):
+        self.objSheet = SheetOps()
+        self.objCommon = CommonFunctions()
+
+    # def cal_outliers(self, lst):
+    #     mn = np.mean(lst)
+    #     sd = np.std(lst)
+    #     final_list = [x for x in lst if (x > mn - 2 * sd)]
+    #     final_list = [x for x in final_list if (x < mn + 2 * sd)]
+    #     list_of_outliers = list(set(lst) - set(final_list))
+    #     # print('outliers:', list_of_outliers)
+    #     return list_of_outliers
+
+    # def cal_change_OI(self, df):
+    #     try:
+    #         df['OI_change'] = 0
+    #         df_length = len(df.index)
+    #         for i, columnData in enumerate(df['OI']):
+    #             s = i + 1
+    #             if s < df_length:
+    #                 temp = columnData - df['OI'].iloc[i + 1]
+    #                 df['OI_change'].iloc[i] = temp
+    #             else:
+    #                 df['OI_change'].iloc[i] = 0
+    #         return df
+    #     except Exception as e:
+    #         print('Exception in changeOI calculation:', e)
+
+    def cal_outliers(self, data, list_ofOI):
+        anomalies = []
+
+        Data_ChOI = [item for item in data if item != 0.0 or item!= 0]
+        Data_OI = [item for item in list_ofOI if item != 0.0 or item != 0]
+        # Set upper and lower limit to 3 standard deviation
+        random_data_std = np.std(Data_ChOI)
+        random_data_mean = np.mean(Data_ChOI)
+        anomaly_cut_off = random_data_std * 2
+
+        lower_limit = random_data_mean - anomaly_cut_off
+        upper_limit = random_data_mean + anomaly_cut_off
+
+        # Generate outliers
+        for outlier in Data_OI:
+            if outlier > upper_limit or outlier < lower_limit:
+                anomalies.append(outlier)
+        return anomalies
+
+    def cal_change_OI(self, df, current_time, symbol, dt, option_type):
+        try:
+            col = df.columns.tolist()
+            if 'OI_change' not in col:
+                df['OI_change'] = 0
+                df['Flag'] = False
+            df_length = len(df.index)
+            # print(list_ofchangeOI)
+
+            for i, columnData in enumerate(df['OI']):
+                s = i + 1
+                if s < df_length:
+                    temp = columnData - df['OI'].iloc[i + 1]
+                    df['OI_change'].iloc[i] = temp
+                else:
+                    df['OI_change'].iloc[i] = 0
+            list_ofchangeOI = df['OI_change'].tolist()
+            list_ofOI = df['OI'].tolist()
+            anomalies = self.cal_outliers(list_ofchangeOI, list_ofOI)
+            if len(anomalies) != 0:
+                for x, y in enumerate(list_ofchangeOI):
+                    for a, b in enumerate(anomalies):
+                        if b == y:
+                            if df['Flag'].iloc[x] == True:
+                                self.notify_process(df, x, symbol, dt, option_type)
+                            df['Flag'].iloc[x] = True
+                            if df['StrTradeDateTime'].iloc[x] == current_time:
+                                self.notify_process(df, x, symbol, dt, option_type)
+            return df
+        except Exception as e:
+            print('Exception in changeOI calculation:', e)
+
+
+    def cal_Out(self, df, current_time, symbol, dt):
+        try:
+            PE = list(df[df['OptionType'] == 'PE']['StrikePrice'].unique())
+            CE = list(df[df['OptionType'] == 'CE']['StrikePrice'].unique())
+            # print(dd)
+            col = df.columns.tolist()
+            if 'OI_change' not in col:
+                df['OI_change'] = 0
+                df['Flag'] = False
+            # df['OI_change'] = 0
+            # df['Flag'] = False
+            # print(df.head())
+            for i, j in enumerate(PE):
+                temp = df[(df.StrikePrice == j) & (df.OptionType == 'PE')]
+                temp = self.cal_change_OI(temp, current_time, symbol, dt, 'PE')
+                # cal_outliers1(j, temp['OI_change'].tolist())
+                for a, row in temp.iterrows():
+                    df['OI_change'].iloc[a] = row['OI_change']
+                    df['Flag'].iloc[a] = row['Flag']
+            for i, j in enumerate(CE):
+                temp = df[(df.StrikePrice == j) & (df.OptionType == 'CE')]
+                temp = self.cal_change_OI(temp, current_time, symbol, dt, 'CE')
+                # print(temp)
+                for a, row in temp.iterrows():
+                    df['OI_change'].iloc[a] = row['OI_change']
+                    df['Flag'].iloc[a] = row['Flag']
+            return df
+        except Exception as e:
+            print('Exception in OUT calculation:', e)
+            return df
+
+    def concate(self, previous_df, df_now, first=True):
+        if first == True:
+            fixed_columns= ['ScripName', 'StrikePrice', 'OptionType',
+                             'StrTradeDateTime', 'TradeDateTime', 'ExpiryDate',
+                             'StrExpiryDate', 'OI', 'COI', 'IV', 'VOL']
+        else:
+            fixed_columns = ['ScripName', 'StrikePrice', 'OptionType',
+                                   'StrTradeDateTime', 'TradeDateTime', 'ExpiryDate',
+                                   'StrExpiryDate', 'OI', 'COI', 'IV', 'VOL', 'OI_change', 'Flag']
+        try:
+            previous_df = self.objCommon.drop_extra_columns(previous_df, fixed_columns)
+            df_now = self.objCommon.drop_extra_columns(df_now, fixed_columns)
+            final = df_now.append(previous_df)
+            return final
+            # final.reset_index(inplace=True)
+        except Exception as e:
+            print('concat exception: ', e)
+
+    def outliers_notify(self, df_now, previous_df, current_time, symbol, dt):
+        try:
+            col = previous_df.columns.tolist()
+            if 'OI_change' in col:
+                if len(previous_df) >= edleConfig.no_of_past_instruments:
+                    value_list = list(previous_df['StrTradeDateTime'].unique())[:edleConfig.no_of_past_instruments]
+                    boolean_series = previous_df.StrTradeDateTime.isin(value_list)
+                    p_df = previous_df[boolean_series]
+                else:
+                    p_df = previous_df
+                # p_df = p_df.loc[:, :'VOL']
+                df_op = self.concate(p_df, df_now)
+                # new_df = self.cal_change_OI(df_op, current_time, symbol, dt)
+                new_df = self.cal_Out(df_op, current_time, symbol, dt)
+                df_now = new_df[:len(df_now)]
+                result = self.concate(previous_df, df_now, False)
+                return result
+            else:
+                # previous_df = previous_df.loc[:, :'VOL']
+                df_op = self.concate(previous_df, df_now)
+                # new_df = self.cal_change_OI(df_op, current_time, symbol, dt)
+                new_df = self.cal_Out(df_op, current_time, symbol, dt)
+                # df_now = new_df[:len(df_now)]
+                # result = self.concate(df_op, df_now, False)
+                return new_df
+        except Exception as e:
+            print('Exception in outliers notify:', e)
+            return previous_df
+
+
+    def notify_process(self, df, x, symbol, expiry_date, option_type):
+        if config.env == 'QA':
+            sheet_to_notify = 'EdelNotifyQA'
+        else:
+            sheet_to_notify = 'EdelweissNotify'
+        try:
+            old_COI = df['COI'].iloc[x + 1]
+            current_coi = df['COI'].iloc[x]
+            dt = df['StrTradeDateTime'].iloc[x]
+            s = df['StrikePrice'].iloc[x]
+            # option_type = df['OptionType'].iloc[0]
+            list_to_write = [dt, symbol, expiry_date, option_type, s, old_COI, current_coi]
+            self.objSheet.writeSheet(config.Notesheet, list_to_write, sheet_to_notify)
+        except Exception as e:
+            print('Exception in outliers Notification Process:', e)
+
+
+
+
+
+
+
+
+
+
+
+
+
+    # def notify_process(self, df, symbol, option_type, expiry_date):
+    #     try:
+    #         df = df[df['OptionType'] == option_type]
+    #         time = df['StrTradeDateTime'].iloc[0]
+    #         StrikePriceList = df[df['StrTradeDateTime'] == time]['StrikePrice']
+    #         sl = StrikePriceList.to_list()
+    #         for s in sl:
+    #             # list_to_write = []
+    #             temp = df[df['StrikePrice'] == s]
+    #             #print(s)
+    #             if len(temp.index) >= 10:
+    #                 temp = temp[:10]
+    #                 # calculate new column
+    #                 temp = self.cal_change_OI(temp)
+    #                 # print(temp.head())
+    #                 current_coi = temp['COI'].to_list()[0]
+    #                 dt = temp['StrTradeDateTime'].to_list()[0]
+    #                 temp.dropna(inplace=True)
+    #                 ll = temp['OI_change'].to_list()
+    #                 old_oi_change = ll[0]
+    #                 # print(old_oi_change)
+    #                 list_of_outliers = self.cal_outliers(ll)
+    #                 # print(list_of_outliers)
+    #                 if len(list_of_outliers) > 0:
+    #                     if old_oi_change in list_of_outliers:
+    #                         old_coi = temp['COI'].to_list()[0]
+    #                         list_to_write = [dt, symbol, expiry_date, option_type, s, old_coi, current_coi]
+    #                         self.objSheet.writeSheet('CIEnotifications', list_to_write, 'EdelweissNotify')
+    #     except Exception as e:
+    #         print('Exception in outliers Notification Process:', e)
+
+    # def outliers_notify_old(self, df, symbol, expiry_date):
+    #     #result, symbol, expiry_date
+    #     try:
+    #         self.notify_process(df, symbol, 'CE', expiry_date)
+    #         self.notify_process(df, symbol, 'PE', expiry_date)
+    #     except Exception as e:
+    #         print('Exception in outliers Notification:', e)
+
+
+
---
+++ Edelweiss_MYSQL_DB/Edelweiss/pEDDB.py
@@ -0,0 +1,255 @@
+from common.gAPI import GoogleAPI
+import pandas as pd
+from Edelweiss.scrapEdDB import ScrapData
+from common.common import CommonFunctions
+from common.DBOperations import DatabaseOp
+from Edelweiss.helpEdDB import HelpEdDB
+import time
+import os
+from pytz import timezone
+import datetime
+import threading
+import Edelweiss.edleConfig as edleConfig
+import config
+import warnings
+warnings.filterwarnings("ignore")
+
+
+class ProcessEd(threading.Thread):
+
+    def concate(self, df_now, previous_df):
+        objCommon = CommonFunctions()
+        fixed_columns = ['ID', 'ScrapedDate', 'ScripName', 'IndexORStocks', 'StrikePrice', 'OptionType', 'StrTradeDateTime', 'TradeDateTime', 'ExpiryDate', 'OI',
+       'COI', 'IV', 'VOL', 'MinuteOI', 'Flag']
+        try:
+            previous_df = objCommon.drop_extra_columns(previous_df, fixed_columns)
+            df_now = objCommon.drop_extra_columns(df_now, fixed_columns)
+            final = df_now.append(previous_df)
+            #final.reset_index(inplace=True)
+        except Exception as e:
+            final = previous_df
+            print('concat exception: ', e)
+
+        return final
+
+    def save_to_drive(self, folder_id, name_of_file, destination):
+        objGAPI = GoogleAPI()
+        try:
+            service = objGAPI.intiate_gdAPI()
+            # Search file id to check it is exists or not
+            # def search_file(service, file_name, mime_type, folder_id, search_in_folder=False):
+            file_id = objGAPI.search_file(service, str(name_of_file), 'text/csv', folder_id, True)
+            if type(file_id) is int:
+                objGAPI.upload_file(service, str(name_of_file), destination, folder_id, 'text/csv')
+            if type(file_id) is str:
+                objGAPI.delete_file(service, file_id)
+                # time.sleep(1)
+                objGAPI.upload_file(service, str(name_of_file), destination, folder_id, 'text/csv')
+            return True
+        except Exception as e:
+            print('Exception while saving files on drive', e)
+            return False
+
+    def endupload(self, symbol, expiry_date, table_name, folder_id):
+        try:
+            exd = expiry_date.replace(' ', '_')
+            file_name = symbol + '_' + exd + '.csv'
+            objHDB = HelpEdDB()
+            objGAPI = GoogleAPI()
+            objCommon = CommonFunctions()
+            result_df, st = objHDB.DB2CSV(symbol, table_name)
+            destination = os.getcwd() + '/Edelweiss/sample_data/' + file_name
+
+            result_df.to_csv(os.getcwd() + '/Edelweiss/sample_data/' + file_name, index=False)
+            service = objGAPI.intiate_gdAPI()
+            isDataAvailable, file_id = objCommon.check_pdata_exist(file_name, folder_id)
+            if isDataAvailable == True:
+                objGAPI.delete_file(service, file_id)
+            objGAPI.upload_file(service, str(file_name), destination, folder_id, 'text/csv')
+        except Exception as e:
+            print('Exception while saving files on drive at the end of day', e)
+            return False
+
+    def process(self, symbol, table_name, expiry_date, iterations,  threshold, pVtime,DB_Name):
+        #objGAPI = GoogleAPI()
+        objScrap = ScrapData()
+        objCommon = CommonFunctions()
+        try:
+            exd = expiry_date.replace(' ', '_')
+            file_name = symbol + '_' + exd + '.csv'
+            status, pVtime = objScrap.start_scraping(str(symbol), expiry_date, threshold, pVtime,DB_Name)
+            if iterations == 30:
+                objHDB = HelpEdDB()
+                if status == True:
+                    result_df, st = objHDB.DB2CSV(symbol, table_name)
+                    # if os.path.exists(os.getcwd() + '/Edelweiss/d_csv/' + file_name):
+                    #     previous_df = pd.read_csv(os.getcwd() + '/Edelweiss/d_csv/' + file_name, index_col=0)
+                    #     result_df = self.concate(current_df, previous_df)
+                    # else:
+                    #     result_df = current_df
+
+                    destination = os.getcwd() + '/Edelweiss/sample_data/' + file_name
+
+                    result_df.to_csv(os.getcwd() + '/Edelweiss/sample_data/' + file_name, index=False)
+                    #service = objGAPI.intiate_gdAPI()
+                    #isDataAvailable, file_id = objCommon.check_pdata_exist(file_name, folder_id)
+                    # if isDataAvailable == True:
+                    #     objGAPI.delete_file(service, file_id)
+                    # objGAPI.upload_file(service, str(file_name), destination, folder_id, 'text/csv')
+                else:
+                    print(f"Scrapping df empty for : {symbol}")
+                    return False, pVtime
+            return True, pVtime
+        except Exception as e:
+            print('Exception in Edle Scrapping Process:', e)
+            return False, pVtime
+
+    def start(self, q, result, isMarketON,  diction,DB_Name): #FolderIDs
+        while not q.empty():
+            work = q.get()
+            if work[1] == 'UPLOAD_THREAD':
+                pass
+                # print('In upload thread')
+                # while True:
+                #     strcurrentTime = datetime.datetime.now(timezone('Asia/Calcutta')).strftime('%H:%M')
+                #     strcurrentTime = strcurrentTime.replace(':', '.')
+                #     print("strcurrentTime===",strcurrentTime)
+                #     #if float(strcurrentTime) > float(15.30):
+                #     if float(strcurrentTime) > float(15.30):
+                #         print('Market is not ON. Try tomorrow or change isMarketON flag')
+                #         break
+                #     for a in range(1800):
+                #         sT = datetime.datetime.now(timezone('Asia/Calcutta')).strftime('%H:%M')
+                #         sT = sT.replace(':', '.')
+                #         #if float(sT) > float(15.30):
+                #         print("st===", sT)
+                #         if float(sT) > float(15.30):
+                #             print('Market is not ON. Try tomorrow or change isMarketON flag')
+                #             break
+                #         time.sleep(1)
+                #     objGAPI = GoogleAPI()
+                #     service = objGAPI.intiate_gdAPI()
+                #     file_id = objGAPI.search_file(service, config.DB_Name, 'mime_type', '1llZZacQjhf2iNPjjpCBSSD4AdKFc5Con', True)
+                #     if file_id != 0:
+                #         objGAPI.delete_file(service, file_id)
+                #     objGAPI.upload_file(service, config.DB_Name, os.getcwd() + '/DB/' + config.DB_Name,
+                #                                 '1llZZacQjhf2iNPjjpCBSSD4AdKFc5Con', 'application/vnd.sqlite3')
+
+            else:
+                try:
+                    if isMarketON == 'TRUE':
+                        threshold = 1.0
+                        ns = threading.local()
+                        ns.iterations = 0
+
+                        #Get threshold
+                        ScrapedFor = work[1]
+                        if diction[ScrapedFor] == 'FALSE':
+                            ScrapedFor = ScrapedFor.split('_')
+                            expDate = ScrapedFor[1]
+                            symbol = ScrapedFor[2]
+                        else:
+                            ScrapedFor = ScrapedFor.split('_')
+                            expDate = ScrapedFor[0]
+                            symbol = ScrapedFor[1]
+                        # objDB = DatabaseOp()
+                        # conn = objDB.create_connection()
+                        # print("query====issue")
+                        # ed = expDate.replace(' ', '-')
+                        # ed = ed.replace('20', '')
+                        # que = "SELECT Threshold FROM Threshold WHERE ScripName='"+symbol+"' AND ExpiryDate='"+str(ed)+"'"
+                        # #que = 'SELECT Threshold FROM Threshold WHERE ScripName=? AND ExpiryDate=?'
+                        # cur = conn.cursor()
+                        # #cur.execute(que, [symbol, str(ed)])
+                        # cur.execute(que)
+                        # rr = cur.fetchone()
+                        # if len(rr) != 0:
+                        #     threshold = rr[0]
+                        # else:
+                        #     threshold = 1.0
+                        #     #print('No threshold existed for given expiry date')
+                        # conn.close()
+                        pVtime = ''
+                        while True:
+                            print('******************* Iterations : ', ns.iterations)
+                            strcurrentTime = datetime.datetime.now(timezone('Asia/Calcutta')).strftime('%H:%M')
+                            strcurrentTime = strcurrentTime.replace(':', '.')
+                            #print("else====vala ==strcurrentTime",strcurrentTime)
+                            #if float(strcurrentTime) > float(15.30):
+                            if float(strcurrentTime) < float(15.30):
+                                print('Market is not ON. Try tomorrow or change isMarketON flag')
+                                exd = expDate.replace(' ', '_')
+                                table_name = config.TableName + exd
+                                #folder_ID = FolderIDs[expDate]
+                                #self.endupload(symbol, expDate, table_name, folder_ID)
+                                break
+                            ScrapedFor = work[1]
+                            if diction[ScrapedFor] == 'FALSE':
+                                ScrapedFor = ScrapedFor.split('_')
+                                expDate = ScrapedFor[1]
+                                symbol = ScrapedFor[2]
+                                #folder_ID = FolderIDs[expDate]
+                                exd = expDate.replace(' ', '_')
+                                table_name = config.TableName + exd
+                                status, pVtime = self.process(symbol, table_name, expDate, ns.iterations,  threshold, pVtime,DB_Name)
+                            else:
+                                ScrapedFor = ScrapedFor.split('_')
+                                expDate = ScrapedFor[0]
+                                symbol = ScrapedFor[1]
+                                #folder_ID = FolderIDs[expDate]
+                                exd = expDate.replace(' ', '_')
+                                table_name = config.TableName + exd
+                                status, pVtime = self.process(symbol, table_name, expDate, ns.iterations,  threshold, pVtime,DB_Name)
+                            if status == True:
+                                ns.iterations += 1
+                            if ns.iterations == 31:
+                                ns.iterations = 0
+                            #Sleep for a minute before next scrapping
+                            time.sleep(59)
+
+                    else:
+                        it = 0
+                        pVtime = ''
+                        strcurrentTime = datetime.datetime.now(timezone('Asia/Calcutta')).strftime('%H:%M')
+                        strcurrentTime = strcurrentTime.replace(':', '.')
+                        #print("third====vala ==strcurrentTime", strcurrentTime)
+                        if float(strcurrentTime) < float(15.30):
+                        #if float(strcurrentTime) > float(15.30):
+                            print('Market is not ON. Try tomorrow or change isMarketON flag')
+                            break
+                        ScrapedFor = work[1]
+                        if diction[ScrapedFor] == 'FALSE':
+                            ScrapedFor = ScrapedFor.split('_')
+                            expDate = ScrapedFor[1]
+                            symbol = ScrapedFor[2]
+                            #folder_ID = FolderIDs[expDate]
+                            exd = expDate.replace(' ', '_')
+                            table_name = config.TableName + exd
+                            status, pVtime = self.process(symbol, table_name, expDate, it,  threshold, pVtime,DB_Name)
+                        else:
+                            ScrapedFor = ScrapedFor.split('_')
+                            expDate = ScrapedFor[0]
+                            symbol = ScrapedFor[1]
+                            #folder_ID = FolderIDs[expDate]
+                            exd = expDate.replace(' ', '_')
+                            table_name = config.TableName + exd
+                            status, pVtime = self.process(symbol, table_name, expDate, it, threshold, pVtime,DB_Name)
+
+                except Exception as e:
+                    print(e)
+                    result[work[0]] = {}
+                # signal to the queue that task has been processed
+            q.task_done()
+        return True
+
+
+#
+# obj = ProcessEd()
+# name_of_file = 'NIFTY_29_Apr_2021.csv'
+# previous_df = pd.read_csv(os.getcwd() + '/d_csv/' + name_of_file, index_col=0)
+# print(previous_df.head(1))
+# df_now = pd.read_csv(os.getcwd() + '/csv/' + name_of_file, index_col=0)
+# print(df_now.head(1))
+# d = obj.concate(previous_df, df_now)
+# print(d.head())
+# print(d.tail())
