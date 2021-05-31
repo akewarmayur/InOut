9b893113abd891a0806700b9f7512c8b722f297f
mayur <mayurakewar12@gmail.com>
2021-05-31 12:11:10 +0530

    sleep 1 sec

--- mainInvestDB.py
+++ mainInvestDB.py
@@ -59,7 +59,7 @@ if __name__ == '__main__':
         # exit eventually even if these dont finish
         # correctly.
         worker.start()
-        time.sleep(7)
+        time.sleep(1)
     # now we wait until the queue has been processed
     q.join()
     logging.info('All tasks completed.')
