026f9abe204125fe8d18800fa42731f3fc1f6fca
VishakhaV2 <36033863+VishakhaV2@users.noreply.github.com>
2021-09-20 15:16:48 +0530

    Changes to getPIDs.py
    Made changes to extract stockCode and PID as it was not working

--- getPids.py
+++ getPids.py
@@ -1,6 +1,20 @@
 import pandas as pd
 import requests
 import re
+from bs4 import BeautifulSoup
+import re
+
+
+#USER_AGENT={'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_10_1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/39.0.2171.95 Safari/537.36'}
+#response = requests.get("https://www.investing.com/equities/acc", headers=USER_AGENT)
+
+#soup = BeautifulSoup(response.text,'html.parser')
+#pidJSON=soup.find('script', id="__NEXT_DATA__").text
+#pid = re.search('instrument_id.....[\d]+', pidJSON ).group(0).replace('instrument_id\\":\\"', '')
+#print(pidJSON)
+#print(pid)
+
+#exit()
 
 def get_pids(link_csv_path):
   stocks = []
@@ -13,44 +27,40 @@ def get_pids(link_csv_path):
   df = pd.read_csv(link_csv_path)
   # print(df.head())
   for i, link in enumerate(df['Stock Links']):
-    # print(link)
-    temp = link.split('/')
-    stock_name = temp[len(temp) - 1].upper()
-    response = requests.get(link, headers=USER_AGENT)
-    dom = response.text
-    # print(dom)
-    result = dom.find('instrumentId')
-    result_name = dom.find('CUSIP')
-    # print(result_name)
-    gg = dom[result_name: result_name + 100]
-    gg = re.sub('[^A-Z]+', '', gg)
-    NiftyCode = gg.replace('CUSIP', '')
-    # remove_lower = lambda text: re.sub('[a-z]', '', text)
-    # gg = remove_lower(gg)
-    print(NiftyCode)
-    ff = dom[result: result + 45]
-    ff = ff.replace('"', '')
-    ff = ff.replace(',', '')
+    try:
+        # print(link)
+        NiftyCode = ""
+        temp = link.split('/')
+        stock_name = temp[len(temp) - 1].upper()
+        response = requests.get(link, headers=USER_AGENT)
+        soup = BeautifulSoup(response.text,'html.parser')
 
-    temp = int(re.findall(r'\d+', ff)[0])
-    # print(temp)
+        pidJSON=soup.find('script', id="__NEXT_DATA__").text
+        pid = re.search('instrument_id.....[\d]+', pidJSON ).group(0).replace('instrument_id\\":\\"', '')
 
-    if (ff.count(stock_name)>0):
-        stocks.append(stock_name)
-    else:
-        stocks.append(stock_name)
-    NCode.append(NiftyCode)
-    pids.append(temp)
+        NiftyCode=soup.select('span[data-test="instrument-bottom-ticker-value"]')[0].text
+        # remove_lower = lambda text: re.sub('[a-z]', '', text)
+        # gg = remove_lower(gg)
+        print(NiftyCode, pid)
+        if (NiftyCode != ""):
+            stocks.append(stock_name)
+        else:
+            stocks.append(stock_name)
+        NCode.append(NiftyCode)
+        pids.append(pid)
 
-    # print(f"Stock=>{stock_name}   PID=>{temp}")
-    if i == 2:
-        break
+        # print(f"Stock=>{stock_name}   PID=>{temp}")
+        #if i == 2:
+        #    break
 
-  stocks_pid['stocks'] = stocks
-  stocks_pid['niftycode'] = NCode
-  stocks_pid['pid'] = pids
-    # break
-  stocks_pid.to_csv('stocks_pids.csv')
+        stocks_pid['stocks'] = stocks
+        stocks_pid['niftycode'] = NCode
+        stocks_pid['pid'] = pids
+        # break
+        stocks_pid.to_csv('stocks_pids.csv')
+    except:
+        pass
+
   return stocks_pid
 
 
@@ -85,7 +95,7 @@ def get_pid(url):
 
 # link_csv_path = "path to csv file contains urls and name"
 # # Enter path of csv It will save as stocks_pids.csv file
-#stocks_pid_df = get_pids('InvestingLinks.csv')
+stocks_pid_df = get_pids('InvestingStocksLinks.csv')
 # print(stocks_pid_df.head())
 #
-# pid, NiftyCode = get_pid('https://in.investing.com/indices/s-p-cnx-nifty')
+#pid, NiftyCode = get_pid('https://in.investing.com/indices/s-p-cnx-nifty')
